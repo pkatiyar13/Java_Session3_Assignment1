2cd8afc299a7f29858d1ab7cdfd7fd4c62a57560
pkatiyar13 <katiyar.cool@gmail.com>
2016-05-06 18:15:20 +0530

    Java_Session3_Assignment1
    Java_Session3_Assignment1

---
+++ S3A1/src/Assignment1.java
@@ -0,0 +1,12 @@
+
+public class Assignment1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int x= 144;
+		int y= 27;
+		System.out.println("Square Root of "+x+" is : "+Math.sqrt(x));
+		System.out.println("Cube Root of "+y+" is : "+Math.cbrt(y));
+	}
+
+}
